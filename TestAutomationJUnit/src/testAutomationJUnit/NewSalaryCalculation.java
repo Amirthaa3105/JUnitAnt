dfdf3b52079cea2fdabbd5fbd9d5e2ecc49499c2
Amirthaananthani Karthi <Amirtha.Karthikeyan@analance.com>
2019-09-26 14:45:26 +0530

    commiting junit ant project first

---
+++ TestAutomationJUnit/src/testAutomationJUnit/NewSalaryCalculation.java
@@ -0,0 +1,8 @@
+package testAutomationJUnit;
+import testAutomationJUnit.EmpDetails;
+public class NewSalaryCalculation {
+	public double calculateNewSalary(EmpDetails emp)
+	{
+		return emp.getEmp_salary()+50000;
+	}
+}
